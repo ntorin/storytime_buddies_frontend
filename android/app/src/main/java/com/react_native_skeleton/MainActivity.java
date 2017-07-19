6b717f9d3fc845cf595a117825deb68d0863c05f
Ntori Nyamekye <ntorikn@gmail.com>
2017-07-19 16:43:09 -0700

    adding react-native-navigation

---
+++ android/app/src/main/java/com/react_native_skeleton/MainActivity.java
@@ -0,0 +1,10 @@
+package com.react_native_skeleton;
+
+import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.controllers.SplashActivity;
+
+
+public class MainActivity extends SplashActivity {
+
+
+}
